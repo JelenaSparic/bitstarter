64660413341a28a8fd8215a0ef38a6e34e0cc92a
Jelena Sparic <jelena.sparic@spanet.co.rs>
2013-07-10 07:25:26 +0000

    Read file and print to buffer

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var BUFFER_LENGHT = 64 * 1024;
+var buffer = new Buffer(BUFFER_LENGTH);
+var src = 'index.html';
+var pos = 0;
+
+bytesRead = fs.readSync(src, buffer, 0, BUFFER_LENGHT, pos);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
