066431e1e25ea07318c416e398a0677c00055e81
Jelena Sparic <jelena.sparic@spanet.co.rs>
2013-07-10 07:41:11 +0000

    Update web.js 2

--- web.js
+++ web.js
@@ -3,15 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var BUFFER_LENGHT = 64 * 1024;
-var buffer = new Buffer(BUFFER_LENGHT);
+var BUFFER_LENGTH = 64 * 1024;
+var buffer = new Buffer(BUFFER_LENGTH);
 var src = 'index.html';
 var pos = 0;
 
-bytesRead = fs.readSync(src, buffer, 0, BUFFER_LENGHT, pos);
+fileOpen = fs.openSync(src,'r');
+bytesRead = fs.readSync(fileOpen, buffer, 0, BUFFER_LENGTH, pos);
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
